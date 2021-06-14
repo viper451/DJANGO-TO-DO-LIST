41e2dccb966d0609cb9a54486af9e25f7bb839e2
viper451 <64985088+viper451@users.noreply.github.com>
2021-06-15 03:37:51 +0530

    Extension one left will be solved for search

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-	path('list.html', views.index, name="list"),
+	path('list.html', views.index, name="list"), 
 	path('update_task/<str:pk>/', views.updateTask, name="update_task"),
 	path('delete/<str:pk>/', views.deleteTask, name="delete"),
 	path('addtask/', views.add_task, name="add_task"),
